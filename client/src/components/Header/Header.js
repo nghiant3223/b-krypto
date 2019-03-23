9ded036076a531c7671452e7be7bd7730a049665
ntncsebku <ntncsebku@gmail.com>
2019-03-23 14:12:43 +0700

    ...

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -12,15 +12,14 @@ const Header = (props) => (
                 <li><NavLink to="/rsa" activeClassName="Header__NavLink--active" className="Header__NavLink">RSA</NavLink></li>
                 <li><NavLink to="/aes" activeClassName="Header__NavLink--active" className="Header__NavLink">AES</NavLink></li>
                 <li><NavLink to="/camellia" activeClassName="Header__NavLink--active" className="Header__NavLink">Camellia</NavLink></li>
-                <li><NavLink to="/keygen" activeClassName="Header__NavLink--active" className="Header__NavLink">Public-key Generator</NavLink></li>
-                <li><NavLink to="/why" activeClassName="Header__NavLink--active" className="Header__NavLink">Why?</NavLink></li>
+                <li><NavLink to="/keygen" activeClassName="Header__NavLink--active" className="Header__NavLink KeyGenNav"> Key Generator</NavLink></li>
             </ul>
         </div>
 
-        <div className="Header__right">
+        {/* <div className="Header__right">
             <button className="Header__right__login">Login</button>
             <button className="Header__right__signup">Sign up</button>
-        </div>
+        </div> */}
     </header>
 );
 
--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -5,7 +5,31 @@ import './HomePage.css';
 class HomePage extends Component {
     render() {
         return (
-            <div className="Homepage">Homepage
+            <div className="Homepage">
+                < div className="MemberContainer" >
+                    <h3 className="MemberContainer__Header">Members</h3>
+                    <ul className="MemberContainer__Content">
+                        <li>1. Nguyễn Gia Bảo</li>
+                        <li>2. Cao Chánh Dương</li>
+                        <li>3. Trần Cảnh Huy</li>
+                        <li>4. Nguyễn Trọng Nghĩa</li>
+                    </ul>
+                </div>
+
+
+                <div className="ProductDescription">
+                    < h3 className="ProductDescription__Header" > Our product</h3>
+                    <div className="ProductDescription__Content">
+                        This is for product description
+                    </div>
+                </div>
+
+                <div className="AlgorithmStrength">
+                    < h3 className="AlgorithmStrength">Algorithms we use</h3>
+                    <div className="AlgorithmStrength__Content">
+                        This is for Algorithms we use
+                    </div>
+                </div>
             </div>
         );
     }
